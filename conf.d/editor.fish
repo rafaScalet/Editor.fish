d0c1f2e18ceed4465392a94598c00bc93d5b761b
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-06-09 15:50:27 -0300

    invert binds

--- conf.d/editor.fish
+++ conf.d/editor.fish
@@ -4,8 +4,8 @@ set -q editor_bind; or set -g editor_bind e
 abbr --add $editor_abbr (__expand_editor_abbr $editor_aditional_args)
 abbr --add $editor_abbr. (__expand_editor_abbr . $editor_aditional_args)
 
-bind --mode insert "ctrl-alt-$editor_bind" "__expand_editor_bind ."
-bind --mode default "ctrl-alt-$editor_bind" "__expand_editor_bind ."
+bind --mode insert "ctrl-shift-$editor_bind" "__expand_editor_bind ."
+bind --mode default "ctrl-shift-$editor_bind" "__expand_editor_bind ."
 
-bind --mode insert "ctrl-shift-$editor_bind" __expand_editor_bind
-bind --mode default "ctrl-shift-$editor_bind" __expand_editor_bind
+bind --mode insert "ctrl-alt-$editor_bind" __expand_editor_bind
+bind --mode default "ctrl-alt-$editor_bind" __expand_editor_bind
